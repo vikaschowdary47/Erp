43f440c6c9114ccf57811611b8e38b68dc9d2493
vikascowdary47 <vikaschowdary47@gmail.com>
2020-08-24 23:05:49 +0530

    sidebarbody

--- src/components/Students/Sidebar/Sidebar.js
+++ src/components/Students/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Sidebar.css";
 
 const Sidebar = () => {
@@ -6,6 +7,20 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="sidebar__body">
         <h3>Students</h3>
+        <ul class="list-group">
+          <li class="list-group-item list-group-item-secondary active">
+            - <Link>View Students</Link>
+          </li>
+          <li class="list-group-item list-group-item-dark">
+            - <Link>Add Student</Link>
+          </li>
+          <li class="list-group-item"></li>
+          <li class="list-group-item"></li>
+          <li class="list-group-item"></li>
+          <li class="list-group-item"></li>
+          <li class="list-group-item"></li>
+          <li class="list-group-item"></li>
+        </ul>
       </div>
     </div>
   );
